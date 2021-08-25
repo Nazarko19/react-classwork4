da89b982b795da6d3d749493cbaa208a21858c9a
Nazarko19 <suhorukyinazar@gmail.com>
2021-08-25 16:56:21 +0300

    classwork4

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,36 +3,40 @@ import {savecar} from "../../servis/save.car.form";
 
 export default function Form() {
 
-    let [model,setModel] = useState('model')
-    let [price, setPrice] = useState('price')
-    let [year,setYear] = useState('year')
-    let [Form,setForm] = useState({model:'',price:'',year:''})
+    // let [model,setModel] = useState('model')
+    // let [price, setPrice] = useState('price')
+    // let [year,setYear] = useState('year')
+    let [form,setForm] = useState({model:'model',price:'price',year:'year'})
 
 
     const onSubmitform = (e) => {
       e.preventDefault()
-        let car={model,price,year}
-        setForm(car)
-        console.log(car)
-        savecar(car)
+        // let car={model,price,year}
+        // setForm(car)
+        // console.log(car)
+        savecar(form)
     }
-const oninputmodel = (e) => {
-        let Model =(e.target.value)
-        setModel(Model)
-}
-const oninputprice = ({target: {value}}) => setPrice(value);
 
-const onninputyear = (e) => {
-        let Year=(e.target.value)
-        setYear(Year)
-
-}
+      const onChange = (e) => {
+        setForm({...form,[e.target.name]:e.target.value})
+      }
+// const oninputmodel = (e) => {
+//         let Model =(e.target.value)
+//         setModel(Model)
+// }
+// const oninputprice = ({target: {value}}) => setPrice(value);
+//
+// const onninputyear = (e) => {
+//         let Year=(e.target.value)
+//         setYear(Year)
+//
+// }
   return (
     <div>
         <form onSubmit={onSubmitform}>
-            <input type="text" name={'model'} value={model} onInput={oninputmodel} />
-            <input type="text" name={'price'} value={price} onInput={oninputprice}/>
-            <input type="text" name={'year'} value={year} onInput={onninputyear}/>
+            <input type="text" name={'model'} value={form.model} onInput={onChange} />
+            <input type="text" name={'price'} value={form.price} onInput={onChange}/>
+            <input type="text" name={'year'} value={form.year} onInput={onChange}/>
             <input type="submit"/>
         </form>
 
